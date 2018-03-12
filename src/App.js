3219b1b03108fce7d45c27720a61ee6db8e2c2da
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-12 16:48:41 +0700

    พื้นฐาน ES6 part 2/2 Rest Parameter, Spread Operator และ Destructuring

--- src/App.js
+++ src/App.js
@@ -9,6 +9,18 @@ class App extends Component {
   }
   render() {
     const sum = 5 + 10
+    const order = {
+      userId: '1',
+      productCode: '001',
+      product: {
+        name: 'Dell',
+        price: '29,500',
+      }
+    }
+
+    const {userId, productCode, product: {name, price}} = order
+    console.warn(userId, productCode, name, price)
+
     return (
       <div className="App">
         <Header title="Test Prop" sum={sum} />
@@ -16,6 +28,9 @@ class App extends Component {
         <button onClick={() => this.handleClick()}>Click 2</button>
         <button onClick={this.newHandleClick}>Click 3</button>
         <button onClick={() => this.newHandleClick2('Name of handle click')}>Click 4</button>
+        
+        
+        <button onClick={ () => this.click('Poom', 'Clicked', 'Samsung', 'Blue')}>Click btn</button> 
       </div>
     )
   }
@@ -28,6 +43,10 @@ class App extends Component {
   }
   newHandleClick2 = (name) => {
     console.log(name, this.state.likeMe)
+  }
+
+  click = (name, ...other) => {
+    console.log('name :', name, 'Other', other)
   }
 }
 
