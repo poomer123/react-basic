c4aeb248c57b4d0357fe2a20228204c2fac6d821
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-12 17:07:52 +0700

    รู้จักและใช้งาน State อย่างถูกวิธี

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+
+  state = { likeMe: 0}
+
+
   render() {
     return (
       <div className="App">
-
+        <h1>{this.state.likeMe}</h1>
+        <button onClick={this.click}>Like</button>
       </div>
     )
+  }
+
+  click = () => {
+    this.setState((prevState) => {
+      return {
+        likeMe: prevState.likeMe + 1
+      }
+    })
   }
 
 }
