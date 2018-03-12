afa1c48d5939ea4e8487dcf9c4152426f4e7ebb7
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-12 15:49:16 +0700

    - create header and import to main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Header from './Header'
 
 class App extends Component {
   render() {
     const sum = 5 + 10
     return (
       <div className="App">
+        <Header />
         {sum}
       </div>
     );
