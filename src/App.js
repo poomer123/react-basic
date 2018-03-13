360984c476580c96551f7084e09bddb201c51a2d
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2018-03-13 14:20:40 +0700

    clean up the file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,9 @@
 import React, { Component } from 'react'
-import Header from './Header'
-import Sidebar from './Sidebar'
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <Header caption="Sample Caption" />
-        <Sidebar>
-          <ul>
-            <li>test 1</li>
-            <li>test 2</li>
-            <li>test 3</li>
-          </ul>
-        </Sidebar>
       </div>
     )
   }
